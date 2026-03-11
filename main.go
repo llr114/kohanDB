cf397033eac23e72b5fb7e7ac4c864bdcde0f651
llr114 <rikuosaki@gmail.com>
2026-03-11 23:19:21 +0900

    add : step 2

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main(){
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("kohanDB> ")
+		scanner.Scan()
+		input_text := scanner.Text()
+		if input_text == ".exit" {
+			break
+		} else {
+			fmt.Println(input_text)
+		}
+	}
 }
